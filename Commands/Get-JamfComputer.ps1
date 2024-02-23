4f7b488c075940981ef4c4bb96c1dd42978d63bd
NineFlint <tom@tcousins.com>
2024-02-23 12:36:33 +1100

    Add comment based help

--- Commands/Get-JamfComputer.ps1
+++ Commands/Get-JamfComputer.ps1
@@ -7,24 +7,52 @@ $get_jamf_computer_all_includes = @(
     'EXTENSION_ATTRIBUTES','CONTENT_CACHING','GROUP_MEMBERSHIPS'
 )
 
+<#
+.SYNOPSIS
+    Get a computer object
 
+.DESCRIPTION
+    A wrapper over the '/api/v1/computers-inventory' endpoint
+
+.OUTPUTS
+    A PSCustomObject that contains the fields specified in the 'Include' parameter. Other properties are present but null.
+
+.EXAMPLE
+    Get-JamfComputer -User 'jsmith@orgname.edu.au'
+    Outputs records of all computers where 'jsmith@orgname.edu.au' is the user
+
+.EXAMPLE
+    Get-JamfComputer -Computer 'C02JJ077DHJW' -Include 'GENERAL','EXTENSION_ATTRIBUTES'
+    Gets the computer matching that serial number, including the specified properties.
+
+.EXAMPLE
+    # user_device_mapping.csv
+    # "Email","Computer"
+    # "jsmith@orgname.edu.au","C02JWM0MDNCR"
+    # "tbrown@orgname.edu.au","C02ZT1NRJV3X"
+
+    Import-Csv "user_device_mapping.csv" | Get-JamfComputer
+#>
 function Get-JamfComputer {
     [CmdletBinding(DefaultParameterSetName='All')]
     Param(
+        # Filters returned computer by either serial number or udid. Note that serial number is not necessarily unique, since there can be zombie enrollments.
         [ValidateNotNullOrEmpty()]
         [Alias('udid')]
         [Parameter(ParameterSetName='Computer',ValueFromPipelineByPropertyName)]
         [String]$Computer,
 
+        # Filters the returned devices by the email or username shown in `userAndLocation`
         [ValidateNotNullOrEmpty()]
         [Parameter(ParameterSetName='User')]
         [String]$User,
 
+        # Custom filter in RSQL format, see Jamf API docs for details
         [ValidateNotNullOrEmpty()]
         [Parameter(ParameterSetName='Filter')]
         [String]$Filter,
 
-        
+        # Which computer object properties to retrieve. A smaller set of properties is retrieved faster. Can use '*' as a shorthand for all properties.
         [ValidateSet(
             '*',
             'GENERAL','DISK_ENCRYPTION','PURCHASING','APPLICATIONS','STORAGE',
@@ -36,6 +64,7 @@ function Get-JamfComputer {
         )]
         [String[]]$Include = @('GENERAL','HARDWARE','USER_AND_LOCATION'),
 
+        # Specify the output type written to the pipeline. "Hashtable" is an OrderedDictionary, "Object" is a PSCustomObject
         [ValidateSet('Hashtable','Object')]
         $As = 'Object'
     )
